4e3f187c4fd65f366385a2986aeaea1a60d7f355
czawora <christopher.zawora@gmail.com>
2019-04-23 10:52:11 -0400

    add print

--- input/input_transfer.sh
+++ input/input_transfer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-globus transfer 45d75c5a-4beb-11e9-a61a-0a54e005f950 e2620047-6d04-11e5-ba46-22000b92c6ec --no-verify-checksum  --sync-level size  --batch --label "2019_04_17_10_39_17 transfer-666sess" < input_batch.txt
+globus transfer deb04de0-fe27-11e8-9345-0e3d676669f4 e2620047-6d04-11e5-ba46-22000b92c6ec --no-verify-checksum  --sync-level size  --batch --label "2019_04_22_17_19_58 transfer-117sess" < input_batch.txt
