7fe42a37d4a8833ca88f5679c6be3ed3547ce7c0
czawora <christopher.zawora@gmail.com>
2019-06-17 14:32:14 -0400

    fix slash

--- output/prepare_output_transfer.py
+++ output/prepare_output_transfer.py
@@ -247,7 +247,7 @@ for idx, src in enumerate(srcs):
 					if os.path.isdir(f):
 						new_batch.write(" --recursive ")
 
-					new_batch.write(sess + "/" + spikeWaveform_path + "" + fname)
+					new_batch.write(sess + "/" + spikeWaveform_path + "/" + fname)
 					new_batch.write(" ")
 					new_batch.write(dest_sess_level + "/sorting/" + fname)
 					new_batch.write("\n")
