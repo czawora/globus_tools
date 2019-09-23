359fe36137372f6fb98e48d5039fff92c1030b7a
czawora <christopher.zawora@gmail.com>
2019-09-23 11:53:27 -0400

    change globus ID

---
+++ input/_input_script_archive/2019_09_05_11_25_57_transfer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+globus transfer 68df8464-cf23-11e9-939e-02ff96a5aa76 e2620047-6d04-11e5-ba46-22000b92c6ec --no-verify-checksum  --sync-level size  --batch --label "2019_09_05_11_25_57 transfer-33sess" < input_batch.txt
--- input/input_transfer.sh
+++ input/input_transfer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-globus transfer 68df8464-cf23-11e9-939e-02ff96a5aa76 e2620047-6d04-11e5-ba46-22000b92c6ec --no-verify-checksum  --sync-level size  --batch --label "2019_09_04_11_15_28 transfer-29sess" < input_batch.txt
+globus transfer 68df8464-cf23-11e9-939e-02ff96a5aa76 e2620047-6d04-11e5-ba46-22000b92c6ec --no-verify-checksum  --sync-level size  --batch --label "2019_09_12_11_04_51 transfer-152sess" < input_batch.txt
